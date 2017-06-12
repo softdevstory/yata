d19e45609c6b0706d1b5f9d59c9b51c09a550706
HS Song <developer@HSui-MacBook-Pro.local>
2017-06-12 10:36:05 +0900

    initial commit

---
+++ yata/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  yata
+//
+//  Created by HS Song on 2017. 6. 12..
+//  Copyright © 2017년 HS Song. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
